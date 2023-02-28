2abb0c348fc55b5949788110dcd03311402c51fa
ivanovaalena <angel40995@inbox.ru>
2023-02-28 23:51:50 +0300

    тест

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,7 +1,8 @@
 import requests
 from pytest_voluptuous import S
 from requests import Response
-from Schema.Schema import user,user_unsuccessful
+from Schema.Schema import user, user_unsuccessful
+
 
 def test_get_users_status_code():
 	response = requests.get(url="https://reqres.in/api/users/2")
@@ -13,15 +14,15 @@ def test_get_users_status_code():
 def test_post_users():
 	response: Response = requests.post(
 		url="https://reqres.in/api/users",
-		json= {
-            "name": "Alena",
-            "job": "QA"
-        })
+		json={
+			"name": "Alena",
+			"job": "QA"
+		})
 	
 	assert response.status_code == 201
 	assert response.json().get("name") == "Alena"
 	assert response.json().get("job") == "QA"
-	
+
 
 def test_post_unknown():
 	response: Response = requests.get(url="https://reqres.in/api/unknown")
@@ -33,27 +34,22 @@ def test_post_register():
 	response: Response = requests.post(
 		url="https://reqres.in/api/register",
 		json={
-             "email": "eve.holt@reqres.in",
-             "password": "pistol"
-              })
+			"email": "eve.holt@reqres.in",
+			"password": "pistol"
+		})
 	assert response.status_code == 200
 	assert response.json().get("id") == 4
 	assert response.json().get("token") == "QpwL5tke4Pnpja7X4"
 
 
 def test_user_unsuccessful():
-    response: Response = requests.post(
-        url="https://reqres.in/api/login",
-        json=
-        {
-            "email": "peter@klaven"
-        }
-    )
-    assert response.status_code == 400
-    assert S(user_unsuccessful) == response.json()
-    assert response.json()["error"] == "Missing password"
-
-	
-	
-
-
+	response: Response = requests.post(
+		url="https://reqres.in/api/login",
+		json=
+		{
+			"email": "peter@klaven"
+		}
+	)
+	assert response.status_code == 400
+	assert S(user_unsuccessful) == response.json()
+	assert response.json()["error"] == "Missing password"
